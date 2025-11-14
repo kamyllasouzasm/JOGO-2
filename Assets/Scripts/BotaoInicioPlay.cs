1a24908d6fc89b3a33a0d7ef4219de83e6d8434c
kamyllasouzasm <145375104+kamyllasouzasm@users.noreply.github.com>
2025-11-14 11:49:41 -0300

    cena inicial feita

--- Assets/Scripts/BotaoInicioPlay.cs
+++ Assets/Scripts/BotaoInicioPlay.cs
@@ -1,16 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BotaoInicioPlay : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void PlayGame()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("SalaDeEspera");
     }
 }
